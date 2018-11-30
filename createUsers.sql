2508ed7c9108b785f8c8e448fc1e1e09800a5c4d
Paul Sinnah <paulsnnh@gmail.com>
2018-11-30 13:20:10 +0100

    Add account page frontend

--- createUsers.sql
+++ createUsers.sql
@@ -1,10 +1,10 @@
-create table users
+CREATE TABLE users
 (
-  id        uuid not null
-    constraint users_pkey
-    primary key,
-  firstname char(64),
-  lastname  char(64),
-  email     char(128),
-  password  char(60)
+    id uuid NOT NULL CONSTRAINT users_pkey PRIMARY KEY,
+    firstname CHAR(64),
+    lastname  CHAR(64),
+    email     CHAR(128),
+    password  CHAR(60)
 );
+
+CREATE UNIQUE INDEX users_email_uindex ON public.users (email);

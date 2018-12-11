3198b3390d26f0bd00c2a0b3379856d4dc845f2c
Paul Development <paulsnnh@gmail.com>
2018-12-11 01:16:49 +0100

    Add profile image change on favorite weapon selection and change favorite skin by favorite city

--- public/js/custom/autocomplete.js
+++ public/js/custom/autocomplete.js
@@ -1,19 +1,57 @@
 $(function() {
-    var favoriteWeapons = [
-        'Fusil d\'assaut',
-        'Fusil d\'assaut à rafale'
+    /* Weapons : Image name */
+    var weapons = {
+        'Fusil d\'assaut (SCAR)': 'scar',
+        'Fusil d\'assaut (M4)': 'm4'
+    };
+
+    /* Add weapons in an array for autocomplete */
+    var favoriteWeapons = [];
+
+    for (var weaponName in weapons) {
+        favoriteWeapons.push(weaponName);
+    }
+
+    var favoriteCities = [
+        'Junk Junction',
+        'Haunted Hills',
+        'Pleasant Park',
+        'Lazy Links',
+        'Loot Lake',
+        'Snobby Shores',
+        'Tilted Towers',
+        'Shifty Shafts',
+        'Polar Peak',
+        'Frosty Flights',
+        'Happy Hamlet',
+        'Risky Reels',
+        'Wailing Woods',
+        'Tomato Temple',
+        'Dsuty Divot',
+        'Lonely Lodge',
+        'Retail Row',
+        'Salty Springs',
+        'Fatal Fiels',
+        'Paradise Palms',
+        'Lucky Landing'
     ];
 
-    var favoriteSkins = [
-        'Patch Patroller',
-        'Pathfinder'
-    ]
-
     $('#favorite-weapon').autocomplete({
-        source: favoriteWeapons
+        source: favoriteWeapons,
+        select: function (event, ui) {
+            var $avatarImg = $('.avatar img');
+
+            if($avatarImg.hasClass('default')) {
+                $avatarImg.removeClass('default');
+            }
+            
+            $avatarImg.attr('src', '/public/img/weapons/' + weapons[ui.item.value] + '.png');
+        }
     });
 
-    $('#favorite-skin').autocomplete({
-        source: favoriteSkins
+    
+
+    $('#favorite-city').autocomplete({
+        source: favoriteCities
     })
 });

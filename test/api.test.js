644cc53960861ea34891b690eb02775747b54ec5
AlexandreArmando <alexandrearmando@hotmail.fr>
2018-12-28 05:38:41 -0800

    Add the first get tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,7 +1,25 @@
 var app = require("../app");
 var request = require("supertest");
-
+var bcrypt = require("bcrypt");
 // close the server after each test
+
+// ----------------------------------------------------------------------------------------------
+
+describe("Test the / get", function() {
+  test("Get / respond 200", function(done) {
+    request(app)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect(200, done);
+  })
+  test("Get /account respond ", function(done) {
+    request(app)
+        .get('/member-area')
+        .set('Accept', 'application/json')
+        .expect(200, done);
+  })
+})
+
 
 describe("Test the login post", () => {
   test("It should set a cookie in header", async () => {
@@ -15,7 +33,7 @@ describe("Test the login post", () => {
       .post("/login")
       .send(result);
     expect(response.statusCode).toBe(302);
-    console.log(response.header["set-cookie"]);
+    //console.log(response.header["set-cookie"]);
     expect(response.header["set-cookie"]).toBeDefined();
     expect(response.header["set-cookie"]).toEqual(expect.arrayContaining([expect.stringMatching("connect.sid=")]));
   });
